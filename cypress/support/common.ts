d73056fc5fe9dfc5559f3587786af067d079795c
David Sveningsson <ext@sidvind.com>
2026-08-21 16:17:49 +0200

    test: disable axe until we can get it working

--- cypress/support/common.ts
+++ cypress/support/common.ts
@@ -1,7 +1,6 @@
 /// <reference types="cypress" />
 
 import "@forsakringskassan/cypress-visual-regression/commands";
-import "@forsakringskassan/cypress-axe/support";
 import "cypress-html-validate/dist/commands";
 import "./commands/forced-colors";
 
