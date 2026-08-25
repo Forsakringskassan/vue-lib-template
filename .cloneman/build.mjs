41736487ae73a63cdc3ec3e58cc737205fd65be6
Carlsson Martin <martin.carlsson@forsakringskassan.se>
2026-08-25 15:13:28 +0200

    feat: add cloneman support

---
+++ .cloneman/build.mjs
@@ -0,0 +1,103 @@
+import pkg from "../package.json" with { type: "json" };
+
+/**
+ * @param {import("cloneman").BuildContext} context
+ */
+export default async (context) => {
+    const { buildTemplate } = context;
+
+    const template = await buildTemplate(pkg.name, {
+        managedFiles: [
+            /* ".agents/**", */
+            ".editorconfig",
+            ".gitignore",
+            ".htmlvalidate.json",
+            ".npmrc",
+            ".prettierignore",
+            /* ".vscode/**", */
+            "api-extractor.lib.json",
+            "api-extractor.selectors.json",
+            "babel.config.js",
+            "cypress/fixtures/example.json",
+            "cypress/support/commands/forced-colors.ts",
+            "cypress/support/common.ts",
+            "cypress/support/component-index.html",
+            "cypress/support/component.ts",
+            "cypress/support/main.scss",
+            "cypress/tsconfig.json",
+            "cypress/types/cy-mount.d.ts",
+            "cypress/types/forced-colors.d.ts",
+            "cypress.config.ts",
+            "docs/build.mts",
+            "docs/package.json",
+            "docs/src/main.scss",
+            "docs/src/setup.ts",
+            "docs/tsconfig.json",
+            "eslint.config.mjs",
+            "Jenkinsfile",
+            "LICENSE.md",
+            "README.md",
+            "renovate.json",
+            "tsconfig.cypress.json",
+            "tsconfig.json",
+            "tsconfig.lib.json",
+            "tsconfig.selectors.json",
+            "vite.config.mts",
+        ],
+        ignoredFiles: [".agents/**", ".vscode/**", ".github/**"],
+        ignoredDependencies: [
+            "@fkui/*",
+            "!@fkui/tsconfig",
+            "@forsakringskassan/*-api",
+            "fk-icons",
+        ],
+    });
+
+    /* install hook to verify application uses the correct template (we cannot put
+     * this in the package `prepare` directly as this project is not managed by
+     * cloneman and thus fails the check). */
+    await template.updateJson("package.json", {
+        scripts: {
+            prepare: ["cloneman verify", pkg.scripts.prepare].join(" && "),
+        },
+    });
+
+    /* remove "cloneman build" from the build script, it is used to test the
+     * template itself not the user application */
+    await template.updateJson("package.json", {
+        scripts: {
+            build: pkg.scripts.build
+                .split(" ")
+                .filter((it) => it !== "build:cloneman")
+                .join(" "),
+            "build:cloneman": undefined,
+        },
+    });
+
+    /* change the semantic-release preset to the one used by user application
+     * instead of publishing cloneman templates */
+    await template.updateJson("package.json", {
+        release: {
+            extends: "@forsakringskassan/semantic-release-config",
+        },
+    });
+
+    await template.writeFile(
+        "CODEOWNERS",
+        [
+            "# This is a CODEOWNERS file.",
+            "# Lines starting with '#' are comments.",
+            "# Each line is a file pattern followed by one or more owners.",
+            "",
+            "# Global owner for the whole repository",
+            "# * @username-or-team",
+            "",
+            "# Example folder-specific owner",
+            "# /docs/ @org/doc-team",
+        ].join("\n"),
+    );
+
+    await template.writeFile("CHANGELOG.md", "");
+
+    await template.renovateIgnoreDependencies();
+};
