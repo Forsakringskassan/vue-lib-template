ae7945845a493da049847ee7b4dbc5defe13ec0e
Wilson Mats (66136604) <mats.wilson@forsakringskassan.se>
2026-09-09 14:46:25 +0200

    test: minor update to the component test (refs SFKUI-6500)

--- src/CatInfoComponent.cy.ts
+++ src/CatInfoComponent.cy.ts
@@ -32,13 +32,15 @@ describe("CatInfoComponent", () => {
 
         // Wait for loading to disappear
         cy.get(catInfoSelectors.loadingContainer()).should("not.exist");
-        cy.get(catInfoSelectors.viewContainer()).should("be.visible");
 
-        cy.contains(testCat.name).should("be.visible");
-        cy.contains(`${testCat.age} år`).should("be.visible");
-        cy.contains(testCat.breed).should("be.visible");
-        cy.contains(testCat.color).should("be.visible");
-        cy.contains(testCat.favoriteFood).should("be.visible");
+        const view = catInfoSelectors.viewContainer();
+        cy.get(view).should("be.visible");
+
+        cy.get(view).contains(testCat.name).should("be.visible");
+        cy.get(view).contains(`${testCat.age} år`).should("be.visible");
+        cy.get(view).contains(testCat.breed).should("be.visible");
+        cy.get(view).contains(testCat.color).should("be.visible");
+        cy.get(view).contains(testCat.favoriteFood).should("be.visible");
     });
 
     it("verifies all fields in edit mode have correct data", () => {
@@ -53,22 +55,20 @@ describe("CatInfoComponent", () => {
         cy.get(catInfoSelectors.loadingContainer()).should("not.exist");
         cy.get(catInfoSelectors.editContainer()).should("be.visible");
 
-        cy.get(catInfoSelectors.nameInput()).should("have.value", testCat.name);
-        cy.get(catInfoSelectors.ageInput()).should(
-            "have.value",
-            testCat.age.toString(),
-        );
-        cy.get(catInfoSelectors.breedInput()).should(
-            "have.value",
-            testCat.breed,
-        );
-        cy.get(catInfoSelectors.colorInput()).should(
-            "have.value",
-            testCat.color,
-        );
-        cy.get(catInfoSelectors.favoriteFoodInput()).should(
-            "have.value",
-            testCat.favoriteFood,
-        );
+        cy.get(catInfoSelectors.nameInput())
+            .should("be.visible")
+            .should("have.value", testCat.name);
+        cy.get(catInfoSelectors.ageInput())
+            .should("be.visible")
+            .should("have.value", testCat.age.toString());
+        cy.get(catInfoSelectors.breedInput())
+            .should("be.visible")
+            .should("have.value", testCat.breed);
+        cy.get(catInfoSelectors.colorInput())
+            .should("be.visible")
+            .should("have.value", testCat.color);
+        cy.get(catInfoSelectors.favoriteFoodInput())
+            .should("be.visible")
+            .should("have.value", testCat.favoriteFood);
     });
 });
